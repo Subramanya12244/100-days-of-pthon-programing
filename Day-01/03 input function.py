8251df86020f3047c307f2c0095f8125f9a1e496
Subramanya12244 <asubramanya370@gmail.com>
2024-10-06 23:29:54 +0530

    commit

---
+++ Day-01/03 input function.py
@@ -0,0 +1,12 @@
+# input()
+input('what you want to')
+var=int(input('what you want to do buds'))
+print('hello '+input())
+
+
+print('----')
+# excersise
+num1 = int(input())
+num2 = int(input())
+
+print(num1*num2)
