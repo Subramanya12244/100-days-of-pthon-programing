48f167908ec79c2302570019c854976e73d4c450
Subramanya12244 <asubramanya370@gmail.com>
2024-10-07 19:11:45 +0530

    commit

---
+++ Day-2/4 BMI calculator.py
@@ -0,0 +1,5 @@
+weight=float(input("enter the weight of the person"))
+height=float(input("enter the height of the person in meters"))
+
+BMI=float(weight/(height**2))
+print("BMI  of the person is",BMI)
