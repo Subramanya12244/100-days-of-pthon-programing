cb06d006f7ea3869726b6b9932cbf02cd3cab18c
Subramanya12244 <asubramanya370@gmail.com>
2024-10-07 19:00:41 +0530

    commit

---
+++ Day-2/3 Mathematical operations.py
@@ -0,0 +1,14 @@
+# addition
+print(2+3)
+# multiplication
+print(2*3)
+# Division
+print(2/3)
+# subtraction
+print(3-2)
+# power
+print(2**3)
+# foloring the division
+print(3//2)
+# modulus
+print(3%2)
