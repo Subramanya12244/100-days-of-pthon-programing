9e4febc83fa5ec3e17cc7f388fdc64456661963b
Subramanya12244 <asubramanya370@gmail.com>
2024-10-07 18:38:08 +0530

    commit

---
+++ Day-2/1 python primitive datatype.py
@@ -0,0 +1,22 @@
+# datatype- string
+
+# subscripting
+
+print("Hello"[0])
+print("Hello"[4])
+
+print("123"+"456")
+# it will just concadinates
+
+# to make it addition we use
+# Integer data type
+
+print(123+456)
+
+# Decimals- Float data type
+
+vip=1.2
+
+# boolea- True/false
+
+temp1=True
