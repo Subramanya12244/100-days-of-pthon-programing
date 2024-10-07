1edd8ef189ee5f1505b2b3a81f1c515f6a56358f
Subramanya12244 <asubramanya370@gmail.com>
2024-10-07 18:44:27 +0530

    commit

---
+++ Day-2/2 type error or type conversion.py
@@ -0,0 +1,9 @@
+num_char=len(input("what's your name "))
+print("your name consists of "+str(num_char)+" characters")
+
+# u get the type error we cannot concat str and int so wee need to convert the type conversion
+
+# but we can concat float and int
+# we can see it in below example as htey are numerical types
+print(70+180.5)
+print(70+float("180.5"))
