00f389e3e65a0fe064edc31e27ba19c88383545b
Subramanya12244 <asubramanya370@gmail.com>
2024-10-14 13:17:44 +0530

    commit

---
+++ Day-2/5 number manipulations and f strings.py
@@ -0,0 +1,14 @@
+import math;
+# we can round off the things and specify the precision.
+print(round(8/3,2));
+
+# floor means it will retuen whole nor and preferably lower whole nor number
+
+print(math.floor(10.8));
+
+# ceil- means it will convert the nor to whole nor highest nearer nor
+
+print(math.ceil(10.1));
+num=10
+# f-strings- can include different datatypes into single string
+print(f"your score is {num}")
