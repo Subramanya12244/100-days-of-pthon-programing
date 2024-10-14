223cb7c37c035f57c2f25854c518f9b8d24ea989
Subramanya12244 <asubramanya370@gmail.com>
2024-10-14 18:08:11 +0530

    commit

---
+++ Day-3 Control and logical operator/1 control flow with if and else with conditional operators.py
@@ -0,0 +1,7 @@
+# if the height of a person is greater than 120ch he can ride if not then he cannot ride.
+
+height=int(input("enter the person's height in cms: "))
+if height>=120:
+    print("you can ride the rollercoaster")
+else:
+    print("Sorry, you cannot ride")
